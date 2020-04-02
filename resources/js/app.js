c5bc8e8226389ba290ce9906d55dbb93cdc10766
Imran Ahamed <imranahamed851999@gmail.com>
2020-04-02 06:25:04 +0600

    Vue Router & Component and fix error

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+
+import {routes} from './routers';
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('master',require('./components/backend/AdminMaster.vue'));
+// Vue.component('dashboard',require('./components/backend/AdminDashboard.vue'));
+
+
+const router = new VueRouter({
+    routes
+});
+
+
+const app = new Vue({
+    el: '#app',
+    router
+});
--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Block | Vue & Laravel</title>
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
@@ -14,7 +15,7 @@
 </head>
 
 <body>
-
+<div id="app">
     <!--*******************
         Preloader start
     ********************-->
@@ -299,6 +300,8 @@
 
             <div class="container-fluid">
 
+                <example-component></example-component>
+
             </div>
             <!-- #/ container -->
         </div>
@@ -322,15 +325,16 @@
     <!--**********************************
         Main wrapper end
     ***********************************-->
-
+</div>
     <!--**********************************
         Scripts
     ***********************************-->
-    <script src="{{asset('js/common.min.js')}}"></script>
+    <script src="{{asset('plugins/common/common.min.js')}}"></script>
     <script src="{{asset('js/custom.min.js')}}"></script>
     <script src="{{asset('js/settings.js')}}"></script>
     <script src="{{asset('js/gleek.js')}}"></script>
     <script src="{{asset('js/styleSwitcher.js')}}"></script>
+    <script src="{{asset('js/app.js')}}"></script>
 
 </body>
 
